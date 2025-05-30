6b18a6350645807ae6a66eff3c0fb58ceeea2cd0
Mide <dicksonmide@gmail.com>
2025-05-30 19:01:15 +0100

    chore: interview assessment end

--- core_skills.py
+++ core_skills.py
@@ -1,6 +1,10 @@
 import random
-# rand_list =
 
-# list_comprehension_below_10 =
+rand_list = []
+print(rand_list)
+for i in range(20):
+    rand_list.append(random.randint(1, 20))
 
-# list_comprehension_below_10 =
+list_comprehension_below_10 = [x for x in rand_list if x < 10]
+
+list_comprehension_below_10 = filter(lambda x: x < 10, rand_list)
--- library/tasks.py
+++ library/tasks.py
@@ -1,7 +1,10 @@
 from celery import shared_task
-from .models import Loan
-from django.core.mail import send_mail
 from django.conf import settings
+from django.core.mail import send_mail
+from django.utils import timezone
+
+from .models import Loan
+
 
 @shared_task
 def send_loan_notification(loan_id):
@@ -10,7 +13,7 @@ def send_loan_notification(loan_id):
         member_email = loan.member.user.email
         book_title = loan.book.title
         send_mail(
-            subject='Book Loaned Successfully',
+            subject="Book Loaned Successfully",
             message=f'Hello {loan.member.user.username},\n\nYou have successfully loaned "{book_title}".\nPlease return it by the due date.',
             from_email=settings.DEFAULT_FROM_EMAIL,
             recipient_list=[member_email],
@@ -18,3 +21,20 @@ def send_loan_notification(loan_id):
         )
     except Loan.DoesNotExist:
         pass
+
+
+@shared_task()
+def check_overdue_loans():
+    due_loans = Loan.objects.filter(
+        due_date__lt=timezone.now(), is_returned=False
+    ).prefetch_related("member", "book")
+    for loan in due_loans:
+        member_email = loan.member.user.email
+        book_title = loan.book.title
+        send_mail(
+            subject="Book Loan Expired!",
+            message=f'Hello {loan.member.user.username},\n\nYou book loan for "{book_title}"  is overdue.\nPlease return it as soon as possible.',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[member_email],
+            fail_silently=False,
+        )
