7edb8cb560090a641193f7ca764999bea1add90b
Mide <dicksonmide@gmail.com>
2025-05-30 19:16:29 +0100

    chore: interview assessment end

--- library_system/urls.py
+++ library_system/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework import routers
+
 from library import views
 
 router = routers.DefaultRouter()
-router.register(r'authors', views.AuthorViewSet)
-router.register(r'books', views.BookViewSet)
-router.register(r'members', views.MemberViewSet)
-router.register(r'loans', views.LoanViewSet)
+router.register(r"authors", views.AuthorViewSet)
+router.register(r"books", views.BookViewSet)
+router.register(r"members", views.MemberViewSet)
+router.register(r"loans", views.LoanViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-]
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+] + router.urls
